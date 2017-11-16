d29b3bf995db3b6e12b105ae17f1ba199d57011f
atanana <tanana@email.ru>
2017-11-16 23:41:11 +0300

    mail subject

--- src/main/scala/com/atanana/vk_digest/UiComposer.scala
+++ src/main/scala/com/atanana/vk_digest/UiComposer.scala
@@ -1,9 +1,16 @@
 package com.atanana.vk_digest
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
 import com.vk.api.sdk.objects.messages.Message
 import play.twirl.api.Html
 
 class UiComposer {
+
+  import UiComposer.DATE_FORMATTER
+
   def composeMail(messages: List[Message]): MailData = MailData(
     messagesHtml(messages),
     subject
@@ -14,7 +21,11 @@ class UiComposer {
     html.main(messagesHtml)
   }
 
-  private def subject: String = ???
+  private def subject: String = LocalDate.now().format(DATE_FORMATTER)
+}
+
+object UiComposer {
+  val DATE_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy", new Locale("ru"))
 }
 
 case class MailData(html: Html, subject: String)
