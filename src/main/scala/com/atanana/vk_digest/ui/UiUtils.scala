96601fe9c7779b601cc5e18c10d3b4c6338ea341
atanana <tanana@email.ru>
2017-11-18 15:31:14 +0300

    messages list styling

--- src/main/scala/com/atanana/vk_digest/ui/UiUtils.scala
+++ src/main/scala/com/atanana/vk_digest/ui/UiUtils.scala
@@ -6,7 +6,7 @@ import java.util.Locale
 
 object UiUtils {
   val SUBJECT_DATE_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy", new Locale("ru"))
-  val MESSAGE_DATETIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm d MM")
+  val MESSAGE_DATETIME_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm d/MM")
 
   def messageDateFormat(date: Int): String = {
     Instant.ofEpochSecond(date).atZone(ZoneId.of("GMT+3")).toLocalDateTime.format(MESSAGE_DATETIME_FORMATTER)
