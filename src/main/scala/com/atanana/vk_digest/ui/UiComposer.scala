df6b72a0577242067034b03e2045be0e6c3d2537
atanana <tanana@email.ru>
2017-11-18 20:16:19 +0300

    show attached messages

--- src/main/scala/com/atanana/vk_digest/ui/UiComposer.scala
+++ src/main/scala/com/atanana/vk_digest/ui/UiComposer.scala
@@ -3,11 +3,12 @@ package com.atanana.vk_digest.ui
 import javax.inject.Inject
 
 import com.atanana.vk_digest.MailConfig
+import com.atanana.vk_digest.vk.UsersProvider
 import com.vk.api.sdk.objects.messages.Message
 import com.vk.api.sdk.objects.users.User
 import play.twirl.api.Html
 
-class UiComposer @Inject()(private val config: MailConfig) {
+class UiComposer @Inject()(private val config: MailConfig, private val usersProvider: UsersProvider) {
 
   def composeMail(messages: List[Message], users: Map[Int, User]): MailData = MailData(
     messagesHtml(messages, users),
@@ -15,11 +16,17 @@ class UiComposer @Inject()(private val config: MailConfig) {
   )
 
   private def messagesHtml(messages: List[Message], users: Map[Int, User]): Html = {
-    val messagesHtml = html.messages(messages, users)
+    val messagesHtml = html.messages(messages, users, this.messages)
     html.main(subject, messagesHtml)
   }
 
   private def subject: String = s"${config.subjectPrefix} ${UiUtils.subjectDate}"
+
+  private def messages(messages: List[Message]): Html = {
+    val userIds = messages.map(_.getUserId.intValue()).distinct
+    val users = usersProvider.users(userIds)
+    html.messages(messages, users, this.messages)
+  }
 }
 
 case class MailData(html: Html, subject: String)
